a281f1eedfbc01d3f8ccc68cc9cfa1e415aadeb3
Pol Gubau <gubaupol@gmail.com>
2022-07-04 12:41:36 +0200

    post created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json());
 let notes = [
   {
     id: 1,
@@ -30,10 +30,45 @@ let notes = [
 app.get("/", (request, response) => {
   response.send("<h1>Hello World</h1>");
 });
-app.get("/notes", (request, response) => {
+app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.send(note);
+  } else {
+    response.status(404).end();
+  }
+});
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
 
+  if (!note || !note.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const ids = notes.map((note) => note.id);
+  const maxId = Math.max(...ids);
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    date: new Date().toISOString(),
+    important: typeof note.important !== "undefined" ? note.important : false,
+  };
+  notes = [...notes, newNote];
+  response.status(201).json(newNote);
+});
+
+//
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id !== id);
+  response.status(204).end();
+});
+
+//
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
